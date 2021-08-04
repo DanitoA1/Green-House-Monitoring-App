5a7797fd9e1805a836642a4507d7cbf1f3c9d3a5
solintin <soliualley@gmail.com>
2021-08-04 01:34:38 +0100

    Green House Monitoring Project Setup

--- src/main.js
+++ src/main.js
@@ -3,5 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import installElementPlus from './plugins/element'
+import './index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+installElementPlus(app)
+app.use(store).use(router).mount('#app')
